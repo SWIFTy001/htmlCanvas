73e6a373d03ec0d92a1e06af697d6d753fdaf02e
Swifty001 <93185257+SWIFTy001@users.noreply.github.com>
2025-06-30 19:15:23 +0500

    first one for chess release

--- canvas.js
+++ canvas.js
@@ -5,6 +5,8 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+
+
 var un = 100;
 var start_x = 4*un;
 var start_y = 0;
@@ -20,12 +22,7 @@ for (var i=1*un ; i<9*un ; i+=un) {
             
         
         c.fillRect(i+start_x,j+start_y,un,un);
- 
 
-        // c.beginPath();
-        // c.arc(((i)*un)+start_x+(un/2),((j+1)*un)+start_y+(un/2),un/2,0,2*Math.PI,true);
-        // c.stroke();
-        // c.strokeStyle = "#fa2ag6";
         
         if (i%(2*un) == 0) {
             j-=un;
@@ -34,17 +31,6 @@ for (var i=1*un ; i<9*un ; i+=un) {
     }
 }
 
-// c.beginPath();
-// c.arc(100,600,30,0,2*Math.PI,true);
-// c.stroke();
 
-//line
-// c.beginPath(200,100);
-// c.moveTo(300,100);
-// c.lineTo(200,100);
-// c.lineTo(150,50);
-// // c.lineTo(1200,100);
-// c.strokeStyle = "#fa2ag6";
-// c.stroke();
 
 console.log(canvas);
